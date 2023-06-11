af96d4b9d611d4b5df146605accee7212fd31352
Stanislav Hristov <101987485+StanislavHristov@users.noreply.github.com>
2023-06-11 22:13:55 +0300

    added cel_to_fahr

---
+++ cel_to_fahr/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STEP 20
+
+int main()
+{
+    const int lower = 0;
+    int upper = 300;
+    int cel;
+    float fahr;
+
+    printf("Celsius to Fahrenheit Converter\n");
+    printf("Celsius\t\tFahrenheit\n");
+
+    for(cel=lower;cel<=upper;cel+=STEP){
+        fahr = cel*9./5 + 32;
+        printf("%d\t%g\n",cel,fahr);
+    }
+    return 0;
+}
