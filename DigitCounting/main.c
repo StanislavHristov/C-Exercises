98473cfed97dab43ca27bfbeb26d7b1421bfb143
Stanislav Hristov <101987485+StanislavHristov@users.noreply.github.com>
2023-06-13 21:44:12 +0300

    Update main.c

--- DigitCounting/main.c
+++ DigitCounting/main.c
@@ -4,23 +4,23 @@
 int main()
 {
     char c;
-    int nWhite=0,nOther=0;
-    int nDigits[]={0,0,0,0,0,0,0,0,0,0};
+    int nWhite = 0, nOther = 0;
+    int nDigits[] = {0,0,0,0,0,0,0,0,0,0};
     int i;
 
-    while((c=getchar())!=EOF){
-        if(c>='0' && c<='9'){
-            nDigits[c-'0']++;
+    while((c = getchar()) != EOF){
+        if(c >= '0' && c <= '9'){
+            nDigits[c - '0']++;
         }
-        else if(c<=' '){
+        else if(c <= ' '){
             ++nWhite;
         } else{
             nOther++;
         }
     }
-    for(i=0;i<10;++i){
-        printf("%d->%d ",i,nDigits[i]);
+    for(i = 0; i < 10; ++i){
+        printf("%d->%d ", i, nDigits[i]);
     }
-    printf("\nWhite spaces: %d\tOther symbols: %d\n",nWhite,nOther);
+    printf("\nWhite spaces: %d\tOther symbols: %d\n", nWhite, nOther);
     return 0;
 }
